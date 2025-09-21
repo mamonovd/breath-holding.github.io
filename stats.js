ead1305009e994508437db2620c9a91ad84ac507
Dmitriy Mamonov <sassy.but.classic@gmail.com>
2025-09-21 20:53:03 +0300

    5th chunk

--- stats.js
+++ stats.js
@@ -88,6 +88,9 @@ const config = (data) => ({
   data: data,
   options: {
     responsive: true,
+    layout: {
+        padding: 40,
+    },
     plugins: {
       legend: {
         position: 'top',
@@ -100,6 +103,22 @@ const config = (data) => ({
   },
 });
 
+function getData() {
+    return new Promise(resolve => { resolve({
+  "stats": [
+    { "d": "2025-05-20", "1": 31, "2": 33, "3": 35, "4": 37, "5": 40 },
+    { "d": "2025-05-21", "1": 31, "2": 34, "3": 34, "4": 36, "5": 35 },
+    { "d": "2025-05-21", "1": 31, "2": 34, "3": 34, "4": 36, "5": 35 },
+    { "d": "2025-05-22", "1": 32, "2": null, "3": null, "4": null, "5": null },
+    { "d": "2025-05-23", "1": 34, "2": 34, "3": null, "4": null, "5": null },
+    { "d": "2025-05-24", "1": 32, "2": 33, "3": 29, "4": 32, "5": 27 },
+    { "d": "2025-05-25", "1": 31, "2": 33, "3": 35, "4": 36, "5": 41 },
+    { "d": "2025-05-27", "1": 31, "2": 32, "3": 33, "4": 34, "5": 35 }
+  ]
+}
+)});
+}
+
 async function init(ctx) {
   let labels = [];
   let datasets = [{
@@ -107,26 +126,36 @@ async function init(ctx) {
     data: [],
     borderColor: '#00988e',
     backgroundColor: '#00988e',
+    cubicInterpolationMode: 'monotone',
+    tension: 0.4,
   },{
     label: "2",
     data: [],
     borderColor: '#4e6ce2',
     backgroundColor: '#4e6ce2',
+    cubicInterpolationMode: 'monotone',
+    tension: 0.4,
   },{
     label: "3",
     data: [],
     borderColor: '#e2ad00',
     backgroundColor: '#e2ad00',
+    cubicInterpolationMode: 'monotone',
+    tension: 0.4,
   },{
     label: "4",
     data: [],
     borderColor: '#582540',
     backgroundColor: '#582540',
+    cubicInterpolationMode: 'monotone',
+    tension: 0.4,
   },{
     label: "5",
     data: [],
     borderColor: '#8bb769',
     backgroundColor: '#8bb769',
+    cubicInterpolationMode: 'monotone',
+    tension: 0.4,
   }];
   (await get('data.json')).stats.forEach(s => {
     labels.push(s.d);
@@ -136,5 +165,8 @@ async function init(ctx) {
   new Chart(ctx, config({labels, datasets}));
 }
 
-init(document.getElementById('myChart'));
+document.addEventListener("DOMContentLoaded", () => {
+  init(document.getElementById('myChart'));
+});
+
 })();
