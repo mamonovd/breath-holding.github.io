c2126144657b417dc8ec3a6fe184ea2c672c8b68
Dmitriy Mamonov <sassy.but.classic@gmail.com>
2026-04-25 11:02:57 +0300

    data 2026-4-25

--- parse.pl
+++ parse.pl
@@ -1,9 +1,17 @@
 while(<>) {
   cmomp;
-  if (/Практики, \[/) {
+  if (/\[([\d\/]+)\s.+\] Практики: ([\d,\s]+)\n/) {
+    @parts = split(/\//, $1);
+    $times = $2 =~ s/,//rg;
+    $times = $times =~ s/\s/, /rg;
+    print '{ "d": "'.'20'.$parts[2].'-'.$parts[1].'-'.$parts[0].'", "t": ['.$times."]},\n";
+  } elsif (/Практики, \[/) {
     $_ = ~/([\d\/]+)/;
     @parts = split(/\//, $1);
   } elsif (/([\d,\s]+)\n/) {
     print '{ "d": "'.'20'.$parts[2].'-'.$parts[1].'-'.$parts[0].'", "t": ['.$1."]},\n";
+  } elsif (/([\d\s]+)\n/) {
+    $times=$1 =~s/\s/, /r;
+    print '{ "d": "'.'20'.$parts[2].'-'.$parts[1].'-'.$parts[0].'", "t": ['.$times."]},\n";
   }
 }
